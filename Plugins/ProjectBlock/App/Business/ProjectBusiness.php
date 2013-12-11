49a672018ccb2db4385b29aa46b32aa17edea764
Antoine Jackson <a.j.william26@gmail.com>
2013-12-11 21:06:35 +0100

    + Added back end code for todoblock, untested yet

--- Plugins/ProjectBlock/App/Business/ProjectBusiness.php
+++ Plugins/ProjectBlock/App/Business/ProjectBusiness.php
@@ -21,6 +21,35 @@ class ProjectBusiness {
     }
     
     public static function createOrUpdate($data) {
-    
+        $project = null;
+        if (isset($data['id'])) {
+            $project = \ProjectBlock\Project::find($data['id']);
+        }
+
+        if (!is_object($project)) {
+            $project = new \ProjectBlock\Project();
+        }
+
+        if (isset($data['name']))
+            $project->setName($data['name']);
+
+        if (isset($data['description']))
+            $project->setDescription($data['description']);
+
+        if (isset($data['tasks']) && is_array($data['tasks'])) {
+            $project->getTasks()->clear();
+            foreach ($data['tasks'] as $tarray) {
+                $task = \TodoBlock\TaskBusiness::createOrUpdate($tarray);
+                if (!$project->getTasks()->contains($task))
+                    $project->getTasks()->add($task);
+            }
+        }
+
+        if ($project->getCreator() == \User::current_user()) {
+            $project->save();
+            return $project;
+        } else {
+            return null;
+        }
     }
 }
--- Plugins/ProjectBlock/App/Controllers/ProjectsController.php
+++ Plugins/ProjectBlock/App/Controllers/ProjectsController.php
@@ -5,25 +5,66 @@
  
 namespace ProjectBlock;
 
+use ProjectBlock\Business\ProjectBusiness;
+
 class ProjectsController extends \Controller {
 
     public function index() {
-    
+        $qb = \Model::getEntityManager()->createQueryBuilder();
+
+        $qb->select('p')->from('\ProjectBlock\Project', 'p')->where('p.creator = :user')->setParameter('user', \User::current_user());
+
+        $result = $qb->getQuery()->getResult();
+
+        $response = array();
+        foreach ($result as $r) {
+            $response[] = $r->toArray();
+        }
+
+        if (\User::current_user() != null) {
+            $this->return_json($response);
+        } else {
+            $this->json_error('You need to be connected');
+        }
     }
     
     public function show($params = array()) {
-    
+        $project = \ProjectBlock\Project::find($params['id']);
+        if (is_object($project) && $project->getCreator() == \User::current_user()) {
+            $this->return_json($project->toArray());
+        } else {
+            $this->json_error('The project was not found', 404);
+        }
     }
     
     public function create($params = array()) {
-    
+        $data = $this->getRequestData();
+        $project = ProjectBusiness::createOrUpdate($data);
+        if (is_object($project)) {
+            $this->return_json($project->toArray());
+        } else {
+            $this->json_error('The project could not be created', 403);
+        }
     }
     
     public function update($params = array()) {
-    
+        $data = $this->getRequestData();
+        $data['id'] = $params['id'];
+        $project = ProjectBusiness::createOrUpdate($data);
+        if (is_object($project)) {
+            $this->return_json($project->toArray());
+        } else {
+            $this->json_error('The project was not found', 404);
+        }
     }
     
     public function destroy($params = array()) {
-        
+        $project = \ProjectBlock\Project::find($params['id']);
+        if (is_object($project) && $project->getCreator() == \User::current_user()) {
+            $project->delete();
+            $this->json_message('The project was successfully deleted');
+        } else {
+            $this->json_error('The project was not found', 404);
+        }
     }
 }
