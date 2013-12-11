72fb6e3414726352f91548a074d4e9186e80cda3
Antoine Jackson <a.j.william26@gmail.com>
2013-12-11 17:39:05 +0100

    + Created Todoblock, created the task model

---
+++ Plugins/TodoBlock/App/Controllers/TasksController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ */
+ 
+namespace TodoBlock;
+
+class TasksController extends \Controller {
+
+    public function index() {
+        
+    }
+    
+    public function show($params = array()) {
+    
+    }
+    
+    public function create($params = array()) {
+    
+    }
+    
+    public function update($params = array()) {
+    
+    }
+    
+    public function destroy($params = array()) {
+        
+    }
+}
